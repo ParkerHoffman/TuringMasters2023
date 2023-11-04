59c424603a0d10db102e593f1d28d796132c1a62
stunnR <112041449+stunnR@users.noreply.github.com>
2023-11-04 15:32:24 -0700

    Changed manifest id

--- life++/background.js
+++ life++/background.js
@@ -1,10 +1,13 @@
+//var ourBrowser = browser; 
 async function testpull() {
-    let onBool = await chrome.storage.sync.get('buttonOn');
+//    let onBool = await ourBrowser.storage.sync.get('buttonOn');
+    let onBool = await browser.storage.sync.get('buttonOn');
     console.log(onBool);
 }
 
 async function testpush(bool) {
-    await chrome.storage.sync.set({
+//    await ourBrowser.storage.sync.set({
+    await browser.storage.sync.set({
         buttonOn: bool
     });
 }
