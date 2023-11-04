c530232309bda608de441e29747838f82033a983
ParkerHoffman <132070255+ParkerHoffman@users.noreply.github.com>
2023-11-04 17:31:20 -0500

    Added a check on teh validity of the API

--- life++/background.js
+++ life++/background.js
@@ -1,10 +1,36 @@
+
+if (typeof browser !== 'undefined' && browser.storage) {
+    // Use the storage API
+    browser.storage.sync.set({
+        'key': 'value'
+    }, function() {
+        if (browser.runtime.lastError) {
+            console.error("Error: " + browser.runtime.lastError);
+        } else {
+            console.log("Data has been saved to sync storage");
+        }
+    });
+} else {
+    console.error("Storage API is not available");
+}
+
+
+
+
+
+
+
+
+
+
+
 async function testpull() {
-    let onBool = await chrome.storage.sync.get('buttonOn');
+    let onBool = await browser.storage.sync.get('buttonOn');
     console.log(onBool);
 }
 
 async function testpush(bool) {
-    await chrome.storage.sync.set({
+    await browser.storage.sync.set({
         buttonOn: bool
     });
 }
