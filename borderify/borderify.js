da4a8ee064ee4700c666a477ff092ca56f6abb5c
stunnR <112041449+stunnR@users.noreply.github.com>
2023-11-03 19:53:37 -0700

    Wrote test alert script
    Took from https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,1 +1,8 @@
-document.body.style.border = "5px solid red";
+document.body.style.border = "5px solid green";
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+await sleep(10*1000)
+alert("This is a test alert.")
