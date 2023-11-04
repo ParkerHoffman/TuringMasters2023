125e19ea959b034aad97acb2c2ebf1e9c2e013bc
ZimmermanTyler <Tnzimmey03@gmail.com>
2023-11-04 11:01:58 -0500

    Combine

--- borderify/popups.js
+++ borderify/popups.js
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-document.body.style.border = "5px solid red";
-=======
 //swal("Hello world!");
 
-<<<<<<< HEAD
-=======
 const commentArray = [
     "Have you done any exercise recently?",
     "Maybe you should call a relative.",
@@ -14,7 +9,6 @@ const commentArray = [
 
 var functionalCommentArray = commentArray.slice();
 
->>>>>>> 6c25488e547e3202cf971b65954d2d0893b8f58c
 function myFunction() {
 /*
     // Set the date we're counting down to
@@ -33,37 +27,21 @@ var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 // Display the result in the element with id="demo"
-<<<<<<< HEAD
-alert(days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s ");
-=======
 alert(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
 */
-var ranCommentNum = Math.floor(Math.random() * commentArray.length)
+var ranCommentNum = Math.floor(Math.random() * functionalCommentArray.length)
 alert(functionalCommentArray[ranCommentNum]);
-if (functionalCommentArray.length != 0) {
+console.log(commentArray)
+console.log(functionalCommentArray)
+if (functionalCommentArray.length != 1) {
     functionalCommentArray.splice(ranCommentNum, 1);
 } else {
+    functionalCommentArray.pop();
     functionalCommentArray = commentArray.slice();
 }
     
->>>>>>> 6c25488e547e3202cf971b65954d2d0893b8f58c
 }
 
 //setTimeout(function() {window.alert("Help");},10000);
-setInterval(myFunction, 10000);
+setInterval(myFunction, 3000);
 
-<<<<<<< HEAD
-//body.innerHTML("<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>');
-
-// contentScript.js (example content script file)// Create a new <script> element
-const sweetAlertScript = document.createElement('script');
-sweetAlertScript.src = 'https://unpkg.com/sweetalert/dist/sweetalert.min.js'; // Replace 'path/to/sweetalert2.js' with the actual path// Append the <script> tag to the head of the page
-document.head.appendChild(sweetAlertScript);
-
-const sweetAlertUses = document.createElement('script');
-sweetAlertUses.innerHTML = 'setInterval(function() {swal("Hello world!");}, 10000);';
-document.body.appendChild(sweetAlertUses);
-=======
->>>>>>> 6c25488e547e3202cf971b65954d2d0893b8f58c
->>>>>>> Stashed changes
