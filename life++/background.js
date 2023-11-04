2d1477d5810ccd463f893e06ddd95ef3483c520c
stunnR <112041449+stunnR@users.noreply.github.com>
2023-11-04 15:10:02 -0700

    Tried chrome in place of browser

--- life++/background.js
+++ life++/background.js
@@ -1,10 +1,10 @@
 async function testpull() {
-    let onBool = await browser.storage.sync.get('buttonOn');
+    let onBool = await chrome.storage.sync.get('buttonOn');
     console.log(onBool);
 }
 
 async function testpush(bool) {
-    await browser.storage.sync.set({
+    await chrome.storage.sync.set({
         buttonOn: bool
     });
 }
